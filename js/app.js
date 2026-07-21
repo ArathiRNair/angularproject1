1a4938600ccce0f485b48c6830b05142e5bbf636
ArathiRNair <arathiravinair@gmail.com>
2026-07-21 12:18:55 +0530

    updated

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app=angular.module("campus360",[]);
+app.controller("dashboardcontroller",function($scope){
+
+    $scope.collegename="FIS";
+    $scope.totalStudents=18500;
+    $scope.totalFaculty=90;
+    
+
+    console.log("dashboard controller is loaded");
+    $scope.students=[
+    "Avany c","Lakshmi krishna","sivani","sandra"
+    ];
+    $scope.addstudent=function(){
+        $scope.totalStudents++;
+    };
+});
